fd0974cd2f5d19ad2f5296ea8d3af9ea0b92db3d
Sebastian K <s3bk@protonmail.com>
2020-10-10 16:27:48 +0300

    update play UI

--- play.js
+++ play.js
@@ -17,14 +17,48 @@ function init() {
         }
     }
     visit(content);
-    function timeupdate(event) {
-        let time = event.target.currentTime;
+
+    let audio = document.getElementById("audio");
+
+    function timeupdate() {
+        let time = audio.currentTime;
         for (mark of marks) {
             let active = (mark.start <= time) && (mark.end >= time);
             mark.element.setAttribute("data-active", active);
         }
     }
-    document.getElementById("audio").addEventListener("timeupdate", timeupdate, false);
+    audio.addEventListener("timeupdate", timeupdate, false);
+
+    let button = audio.parentElement.getElementsByTagName("button")[0];
+
+    function pause() {
+        button.className = "play";
+        audio.pause();
+        playing = false;
+    }
+    function play() {
+        button.className = "pause";
+        audio.play();
+        playing = true;
+    }
+    function toggle() {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    }
+    function update_button() {
+        button.className = ["pause", "play"][audio.paused | 0];
+    }
+    audio.addEventListener("ended", update_button, false);
+    audio.addEventListener("play", update_button, false);
+    audio.addEventListener("pause", update_button, false);
+
+    button.addEventListener("click", toggle, false);
+
+    audio.style.display = "none";
+    button.style.display = "block";
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
